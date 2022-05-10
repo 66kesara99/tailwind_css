4b2d4c84fcded5b2d405310cff0c7b4693002dbc
Kesara Gamlath <66kesara99@gmail.com>
2022-05-11 01:08:16 +0530

    Tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className=" flex justify-center">
+        <div className="bg-blue-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-blue-400 rounded-lg hover:scale-110"></div>
+        <div className="bg-red-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-red-400 rounded-lg animate-pulse"></div>
+        <div className="bg-green-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-green-400 rounded-lg hover:skew-y-12"></div>
+      </div>
+      <div className=" flex justify-center">
+        <div className="bg-red-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-red-400 rounded-lg animate-pulse cursor-pointer"></div>
+        <div className="bg-green-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-green-400 rounded-lg animate-spin "></div>
+        <div className="bg-red-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-red-400 rounded-lg animate-pulse"></div>
+      </div>
+      <div className=" flex justify-center">
+        <div className="bg-green-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-green-400 rounded-lg hover:rotate-45"></div>
+        <div className="bg-red-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-red-400 rounded-lg animate-pulse"></div>
+        <div className="bg-blue-700 w-40 h-40 mx-4 my-4 hover:ring-8 hover:ring-blue-400 rounded-lg hover:translate-y-6 "></div>
+      </div>
     </div>
   );
 }
